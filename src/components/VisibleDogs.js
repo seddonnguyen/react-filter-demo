ecf07cd52371dce4470e4b326da140ed4bf3042f
github username <github email address>
2019-08-12 14:55:10 -0600

    Done

--- src/components/VisibleDogs.js
+++ src/components/VisibleDogs.js
@@ -5,12 +5,12 @@ const VisibleDogs = (props) => {
     console.log("visible dogs", props)
     return (
         <div>
-
-
-            <h1>{ props.dog.name }</h1>
+            {/* put dog name in h1 tag */ }
+            <h1>{props.dog.name}</h1>
             {/* put breed's purpose in p tag */ }
-
+            <p>{props.dog.bred_for}</p>
             {/* put breed's origin in p tag */ }
+            <p>{props.dog.life_span}</p>
 
         </div>
     )
